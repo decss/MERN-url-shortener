d3a6353da9c1c336bd6095d1a069301c1320d5a6
Yaroslav <decss.j@gmail.com>
2020-05-04 16:01:07 +0400

    Database connection

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('  Start application')
+console.log('Start application')
+
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const PORT = config.get('port') || 5000
+const app = express()
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        console.log('  Connected to database')
+
+        app.listen(PORT, () => console.log(`  Listening port ${PORT} ...`))
+    } catch (e) {
+        console.log(`Server Error:`, e.message)
+        process.exit(1);
+    }
+}
+
+start()
