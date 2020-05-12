668a2986547ea2bc3d727ba64ccff3946ee5ce08
Yaroslav <decss.j@gmail.com>
2020-05-12 19:25:48 +0400

    Server: Auth login hook

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import logo from './logo.svg'
 import React from 'react'
 import {BrowserRouter as Router} from "react-router-dom";
 import {useRoutes} from "./routes"
+import {useAuth} from "./hooks/auth.hook";
 import 'materialize-css'
 
 function App() {
+    const {token, login, logout, userId} = useAuth()
     const routes = useRoutes(false)
     return (
         <Router>
