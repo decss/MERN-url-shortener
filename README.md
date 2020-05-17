14f65cb1b20410c64fcaa88c48f39a771e581b4c
Yaroslav <decss.j@gmail.com>
2020-05-17 21:37:54 +0400

    Pre-deploy

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 Url shortener application on MERN. 
 
-#### Some specs
-Frontend port: 3000<br />
-Backend port: 5000
+You can see working example here: http://3.121.130.216:5000 <br>
+Demo user: **demo@gmail.com**<br>
+password: **pass**
+
+#### Some details
+Frontend port: 5000<br>
+Backend port: 3000<br>
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('Start application')
 
 const express = require('express')
 const config = require('config')
+const path = require('path')
 const mongoose = require('mongoose')
 
 const PORT = config.get('port') || 5000
@@ -11,6 +12,14 @@ app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/rediret.routes'))
+
+if (process.env.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 async function start() {
     try {
