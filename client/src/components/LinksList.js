48132238b0c59dcb1cc00d22a5386f8cf16a2a51
Yaroslav <decss.j@gmail.com>
2020-05-19 21:07:59 +0400

    Client: add links list page

---
+++ client/src/components/LinksList.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+export const LinksList = ({links}) => {
+    if (!links || !links.length) {
+        return <p className="center">There is no links yet</p>
+    }
+
+    return (
+        <div className="row">
+            <div className="col s12 m8 offset-m2 xl6 offset-xl3" style={{paddingTop: '2rem'}}>
+                <h3>Links list</h3>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Shorten</th>
+                        <th>Target</th>
+                        <th></th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    { links.map((link, index) => {
+                        return (
+                            <tr>
+                                <td>{index + 1}</td>
+                                <td>{link.to}</td>
+                                <td>{link.from}</td>
+                                <td>
+                                    <Link to={`detail/${link._id}`}>Show info</Link>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/LinksPage.js
+++ client/src/pages/LinksPage.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, {useCallback, useContext, useState, useEffect} from 'react'
+import {useParams} from "react-router-dom";
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContext";
+import {Loader} from "../components/Loader";
+import {LinksList} from "../components/LinksList";
+
 
 export const LinksPage = () => {
+    const {token} = useContext(AuthContext)
+    const [links, setLinks] = useState(null)
+    const {request, loading} = useHttp()
+
+    const fetchLink = useCallback(async () => {
+        try {
+            const fetched = await request('/api/link', 'GET', null, {
+                Authorization: `Bearer ${token}`
+            })
+            setLinks(fetched)
+        } catch (e) {
+        }
+    }, [token, request])
+
+    useEffect(() => {
+        fetchLink()
+    }, [fetchLink])
+
+    if (loading) {
+        return <Loader/>
+    }
+
     return (
-        <div className="container">
-            <h1>Links</h1>
-        </div>
+        <>
+            {!loading && <LinksList links={links}/>}
+        </>
     )
 }
